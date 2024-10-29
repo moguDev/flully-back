9df8c4f49f3b6f0f62ac2fbd1d5a681c7dffc912
moguDev <mail@sho-horiguchi.jp>
2024-10-29 15:46:14 +0900

    ルーティングの変更(/api/v1をつけた)、ユーザ情報」登録時のパラメータの許可(name, nickname)

---
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -0,0 +1,10 @@
+class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
+  private
+  def sign_up_params
+    params.permit(:email, :password, :password_confirmation, :name, :nickname)
+  end
+
+  def account_update_params
+    params.permit(:email, :password, :password_confirmation, :name, :nickname)
+  end
+end
