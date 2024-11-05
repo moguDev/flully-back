73162673e0388cf9305e85a3df96d18bae3a13d7
moguDev <mail@sho-horiguchi.jp>
2024-11-06 00:10:54 +0900

    entrypoint.shの修正

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,9 +1,9 @@
 class AvatarUploader < CarrierWave::Uploader::Base
-  # if Rails.env.production?
+  if Rails.env.production?
     storage :fog
-  # else
-  #   storage :file
-  # end
+  else
+    storage :file
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
