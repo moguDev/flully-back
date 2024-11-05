34f4806daea882d875d5cc5938be90ba548f73ab
moguDev <mail@sho-horiguchi.jp>
2024-11-06 00:28:47 +0900

    entrypoint.shの修正

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,9 @@ set -e
 
 rm -f /usr/src/app/tmp/pids/server.pid 
 
-bundle exec rails db:migrate
+if [ "$RAILS_ENV" = "production" ]; then
+  bundle exec rails db:create
+  bundle exec rails db:migrate
+fi
 
 exec "$@"
