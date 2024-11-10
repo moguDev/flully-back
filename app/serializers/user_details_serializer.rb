d484ff4493555718945f4c212bceeef309a864e5
moguDev <mail@sho-horiguchi.jp>
2024-11-10 17:59:10 +0900

    現在の継続散歩日数の算出

--- app/serializers/user_details_serializer.rb
+++ app/serializers/user_details_serializer.rb
@@ -1,5 +1,5 @@
 class UserDetailsSerializer < ActiveModel::Serializer
-  attributes :id, :name, :nickname, :introduction, :avatar_url, :email, :location, :twitter
+  attributes :id, :name, :nickname, :introduction, :avatar_url, :email, :location, :twitter, :current_streak
 
   has_many :boards, each_serializer: BoardSerializer
   has_many :posts, each_serializer: PostSerializer
@@ -7,5 +7,20 @@ class UserDetailsSerializer < ActiveModel::Serializer
 
   def avatar_url
     object.avatar.url if object.avatar.present?
+  end
+
+  def current_streak
+    today = Time.current.in_time_zone('Asia/Tokyo').to_date
+    walk_dates = object.walks.map { |walk| walk.start_time.to_date }
+    walk_dates = walk_dates.uniq
+    streak = 0
+    current_date = today
+
+    while walk_dates.include?(current_date)
+      streak += 1
+      current_date = current_date - 1.day # 前日へ
+    end
+
+    streak
   end
 end
