4b578c0f7c5a2263087ab2880128f16548028e23
moguDev <mail@sho-horiguchi.jp>
2024-12-04 00:34:10 +0900

    few fix

--- app/mailers/devise/mailer.rb
+++ app/mailers/devise/mailer.rb
@@ -2,7 +2,7 @@ class Devise::Mailer < ApplicationMailer
   def reset_password_instructions(record, token, opts={})
     @token = token
     @resource = record
-    @reset_password_url = "https://flully.jp/reset_password?reset_password_token=#{token}"
+    @reset_password_url = "https://flully.jp/reset_password/#{token}"
 
     mail(to: record.email, subject: 'Password Reset Instructions')
   end
